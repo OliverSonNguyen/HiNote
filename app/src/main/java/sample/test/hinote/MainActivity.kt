0f9919cd6d2f13265f4a50dbd6c361f9327c1076
nguyenthanh.son <nguyenthanh.son@okg.com>
2024-07-02 12:58:39 +0800

    enable viewbinding

--- app/src/main/java/sample/test/hinote/MainActivity.kt
+++ app/src/main/java/sample/test/hinote/MainActivity.kt
@@ -2,10 +2,13 @@ package sample.test.hinote
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import sample.test.hinote.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
